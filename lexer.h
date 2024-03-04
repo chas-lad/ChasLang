5dbfea5119dda96ed66e039efc63c2142e04e54f
Chas Ladhar <137926064+chas-lad@users.noreply.github.com>
2024-03-04 11:58:29 +0000

    seperated files

---
+++ lexer.h
@@ -0,0 +1,21 @@
+#ifndef LEXER_H
+#define LEXER_H
+
+typedef enum {
+    INT,
+    KEYWORD,
+    SEPERATOR,
+    END_OF_TOKENS
+} TokenType;
+
+typedef struct {
+    TokenType type;
+    char *value;
+} Token;
+
+Token *generate_number(char *current, int *current_index);
+Token *generate_keyword(char *current, int *current_index);
+void print_token(Token token);
+Token *lexer(FILE *file);
+
+#endif
